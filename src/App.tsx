55ab2b6f2b1f24899da82d1a9e0c2ab8ebef1505
Jordan Sinou <sinoujordan@gmail.com>
2023-03-15 17:52:01 +0100

    remove warns

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
 import React, { useState } from 'react';
 import './style/main.scss'
-import Button from "./atoms/Button";
-import MenuItem from './molecules/MenuItem'
-import Avatar from './atoms/Avatar';
-import Spacer from './atoms/Spacer';
-import Subtitle from './atoms/fonts/Subtitle';
-import Textinfo from './atoms/fonts/TextInfo';
-import SubtitleInfo from './molecules/TitleInfo';
-import Textcontent from './atoms/fonts/TextContent';
 import SideMenu from './components/SideMenu';
 import { Theme } from './interfaces/Theme';
 
